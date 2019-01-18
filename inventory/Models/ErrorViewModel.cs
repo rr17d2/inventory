d7394af744a5ee735eaa3c02e6334fe1ee9df9e3
rr17d2 <nikefutbol90@gmail.com>
2019-01-18 02:09:00 -0800

    1st progress
    Set up the MS SQL Server and started applying the database for the application by creating tables and entities.

---
+++ inventory/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace inventory.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
